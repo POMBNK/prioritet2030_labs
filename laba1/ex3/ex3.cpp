079a90e428531783a4cedd70bb8d9279e6efe8aa
pombnk <shardex11@gmail.com>
2022-10-10 12:09:08 +0300

    add ex3.cpp

--- laba1/ex3/ex3.cpp
+++ laba1/ex3/ex3.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
+#include <ostream>
 #include <sys/types.h>
 
 int main() {
   u_int c0;
-  std::cout<<"Введите любое неотрицательное число больше нуля";
-  std::cin>>c0;
+  int step = 0;
+  std::cout << "Введите любое неотрицательное число больше нуля\n";
+  std::cin >> c0;
   if (c0 <= 0) {
-    std::cout<<"Вы ввели неверное значение";
+    std::cout << "Вы ввели неверное значение";
   }
-  if (c0%2==0) {
-    c0 = c0/2; 
-  } else {
-    c0 = 3*c0+1;
+  while (c0 != 1) {
+    if (c0 % 2 == 0) {
+      c0 = c0 / 2;
+    } else {
+      c0 = 3 * c0 + 1;
+    }
+    std::cout << c0 << std::endl;
+    step++;
   }
+  std::cout << "steps:" << step << std::endl;
 }
