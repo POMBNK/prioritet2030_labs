c704bafb16aa245a7a9f111bd4494a18b3ed93d6
POMBNK <shardex11@gmail.com>
2022-10-26 00:05:59 +0300

    add laba exercsices

---
+++ laba3/ex3.1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Good{
+   int count;
+   double price;
+   double weight;
+   string name;
+};
+
+int main() {
+  int n = 2;
+  vector<Good> goods(n);
+  goods[0].count=3;
+  //Done this later
+}
+
+
+
